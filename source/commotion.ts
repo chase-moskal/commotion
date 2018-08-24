d6b286d0d9504c5d5f63569c41d777e90b23fb21
Chase Moskal <chasemoskal@gmail.com>
2018-08-23 17:17:30 -0700

    update packages and erorr handling

--- source/commotion.ts
+++ source/commotion.ts
@@ -1,7 +1,7 @@
 
 /*
 
-commotion — communications functions like xhr's
+commotion — browser communication functions, xhr's
 
 */
 
@@ -10,27 +10,30 @@ export interface RequestParams {
 }
 
 /**
- * Make an XHR request
+ * make and http get request via xhr
  */
 export function request({link}: RequestParams): Promise<XMLHttpRequest> {
 	return new Promise<XMLHttpRequest>((resolve, reject) => {
 		const request = new XMLHttpRequest()
 		request.onload = () => resolve(request)
-		request.onerror = event => reject(event.error)
+		request.onerror = event => {
+			const error = new Error(`xhr request error: ${request.status} ${request.statusText}`)
+			reject(error)
+		}
 		request.open("GET", link)
 		request.send()
 	})
 }
 
 /**
- * Make an XHR request for an XML document
+ * make an xhr request for an xml document
  */
 export async function requestXml(params: RequestParams) {
 	return (await request(params)).responseXML
 }
 
 /**
- * Make an XHR request for a JSON document
+ * make an xhr request for a json document
  */
 export async function requestJson(params: RequestParams) {
 	return JSON.parse((await request(params)).responseText)
